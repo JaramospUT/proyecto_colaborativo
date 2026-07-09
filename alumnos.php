2e71573c73b363b94e77ff601fdf82ac7d93cfcd
JaramospUT <julian.ramos@utl.edu.mx>
2026-07-08 19:50:43 -0600

    Cafeterìa

--- alumnos.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Jaciel Ramirez";
-echo "Alexis Gabriel";
-echo "Juan Manuel";
-echo "Endrik Gomez";
-echo "Julian Ramos";
-echo "Anthony";
--- index.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "Hola mi nombre es: Juan Manuel y colabore con endrick";
