06fa5a570a2ca80d5dc8027199a2fb83ea01a645
Manu-Art4444 <manuelperales2312@gmail.com>
2026-07-10 20:40:37 -0600

    env agregado

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Leer JSON
 app.use(express.json());
